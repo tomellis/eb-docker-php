8353c2cee3726aac4275addf5226065d4c793d02
Tom Ellis <tom@trellisnet.co.uk>
2016-02-08 22:49:05 +0000

    eb create script

---
+++ eb-create.sh
@@ -0,0 +1,1 @@
+eb create eb-docker-php-dev2 --tags user=trellis --vpc.id vpc-28f9ce4d --vpc.ec2subnets subnet-baa2fbe3,subnet-24494953 --vpc.elbpublic --vpc.elbsubnets subnet-25494952,subnet-bba2fbe2
